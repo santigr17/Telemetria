317e940fc847584392e14c7e2a98fb9f44a8f69c
Santigr17 <santigr17@gmail.com>
2019-03-20 17:17:26 -0600

    mejorando la interfaz

--- ClientePython/TelemetryLog.py
+++ ClientePython/TelemetryLog.py
@@ -11,15 +11,11 @@ import os                           # ruta = os.path.join('')
 import time                         # time.sleep(x)
 from tkinter import messagebox      # AskYesNo ()
 
-
-#           ____________________________
-#__________/Función para cargar imagenes
-def cargarImg(nombre):
-    ruta=os.path.join('img',nombre)
-    imagen=PhotoImage(file=ruta)
-    return imagen
-
-
+global connCar, pressing, power, dire, lights
+connCar, pressing = False, False
+power = 0;
+dire = 0;
+lights = '0b11111111'
 
 #           ____________________________
 #__________/Ventana Principal
@@ -38,78 +34,80 @@ C_root.place(x=0,y=0)
 
 L_titulo=Label(C_root,text=about,font=('Agency FB',14),bg='white',fg='black')
 L_titulo.place(x=550,y=10)
+
 #           _____________________________________
 #__________/Se crea una entrada de texto y titulo
-L_ingresarNombre = Label(C_root,text="Ingrese su nombre:",font=('Agency FB',14),bg='white',fg='green')
-L_ingresarNombre.place(x=560,y=400)
-E_nombre = Entry(C_root,width=20,font=('Agency FB',14))
-E_nombre.place(x=560,y=425)
+L_ingresarCommand = Label(C_root,text="Comando:",font=('Agency FB',14),bg='white',fg='green')
+L_ingresarCommand.place(x=30,y=500)
+
+E_Command = Entry(C_root,width=20,font=('Agency FB',14))
+E_Command.place(x=100,y=500)
+
+SentCarText = Text(C_root, height=3, width=45)
+SentCarText.place(x=10,y=50)
+SentCarText.insert(END, "Text of sending messages to car\nJust a text Widget\nin two lines\n")
+
+RevCarText = Text(C_root, height=3, width=45)
+RevCarText.place(x=400,y=50)
+RevCarText.insert(END, "Text of sending messages to car\nJust a text Widget\nin two lines\n")
 
 
-#           ____________________________
-#__________/Crear una nueva ventana
-def VentanaJuego(nombre_jugador):
-    #Esconder la pantalla sin destruirla
-    #root.withdraw()
-    #Pantalla secundarias
-    juego=Toplevel()
-    juego.title('EJEMPLO')
-    juego.minsize(800,600)
-    juego.resizable(width=NO, height=NO)
+def active(key):
+    global pressing
+    while(pressing):
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.connect(car_address)
+            mns = getValues()
+            mns += "\r"
+            sock.sendall(mns.encode())
+        except Exception as e:
+            print(str(e))
+        finally:
+            sock.close()
+            time.sleep(0.1)
 
-    C_juego=Canvas(juego, width=800,height=600, bg='light blue')
-    C_juego.place(x=0,y=0)
 
-
-    L_nombre=Label(C_juego, text="Piloto: "+nombre_jugador ,font=('Agency FB',20), fg='light blue', bg='white')
-    L_nombre.place(x=10,y=10)
-
-    CarText = Text(C_juego, height=3, width=45)
-    CarText.place(x=10,y=50)
-    CarText.insert(END, "Text of sending messages to car\nJust a text Widget\nin two lines\n")
     
-    ControlText = Text(C_juego, height=3, width=45)
-    ControlText.place(x=400,y=50)
-    ControlText.insert(END, "Text of receiving messages from control\nJust a text Widget\nin two lines\n")
+def keyPress(event):
+    global pressing
+    pressing = True
     
-#           _____________________________
-#__________/Volver a la ventana principal
-    def back():
-        juego.destroy()
-        #root.deiconify()
+    q=Thread(target=active,args=())
+    q.start()
 
 
-#           ____________________________
-#__________/Boton volver pantalla juego
+def keyRelease(event):
+    global pressing
+    pressing = False
+    goDefault()
+        
+    
 
-    Btn_back = Button(C_juego,text='Atras',command=back,bg='white',fg='green')
-    Btn_back.place(x=100,y=560)
+root.bind_all('<KeyPress>', keyPress)
+root.bind_all('<KeyRelease>', keyRelease)
 
 
-
-#           ____________________________
-#__________/Función del botón juego
-def empezar_juego():
-    #Obtener el nombre de un entry
-    nombre = str(E_nombre.get())
-    VentanaJuego(nombre)
-
+def log():
+    pass
 
 def conCar():
+    global Car
+    Car = True
+
+def send():
     pass
 
-def conControl():
-    pass
 #           ____________________________
 #__________/Botones de ventana principal
 
-Btn_hilos = Button(C_root,text='Log',command=empezar_juego,fg='white',bg='green')
-Btn_hilos.place(x=560,y=470)
+Btn_hilos = Button(C_root,text='Print Log',command=log,fg='white',bg='green')
+Btn_hilos.place(x=200,y=10)
 
-Btn_ConnectCar = Button(C_root,text='Car Connection',command=conCar,fg='white',bg='green')
-Btn_ConnectCar.place(x=100,y=550)
+Btn_ConnectCar = Button(C_root,text='Start Control',command=conCar,fg='white',bg='green')
+Btn_ConnectCar.place(x=50,y=10)
 
-Btn_ConnectControl = Button(C_root,text='Control Connection',command=conControl,fg='white',bg='green')
-Btn_ConnectControl.place(x=200,y=550)
+Btn_ConnectControl = Button(C_root,text='Enviar',command=send,fg='white',bg='green')
+Btn_ConnectControl.place(x=250,y=500)
 
 root.mainloop()
