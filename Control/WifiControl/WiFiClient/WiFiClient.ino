173a76e75bfa03419300537eabab274f36ea4733
Roberto Gutiérrez Sánchez <22207159+robertogs2@users.noreply.github.com>
2019-04-26 18:11:47 -0600

    Agrego boton para pito

--- Control/WifiControl/WiFiClient/WiFiClient.ino
+++ Control/WifiControl/WiFiClient/WiFiClient.ino
@@ -11,7 +11,7 @@
 #include <Wire.h>
 #include <ESP8266WiFi.h>
 
-//Constants for network connection 
+//Constants for network connection
 const char* ssid      = "WiFiCar2";
 const char* password  = "pass1234";
 const char* host      = "192.168.43.252";
@@ -21,70 +21,77 @@ const int   port      = 8010;
 MPU9250     myIMU;
 WiFiClient  client;
 
-//Task constants and variables
+//Task constants and variables for network
 const uint8_t           maxAttempsNetwork = 20;
 const uint8_t           maxAttempsHost    = 20;
 const unsigned long int interval          = 50;
+
+//Variables for buttons
+//int button0Pin = 0; // RX
+int button1Pin = 1; // TX
+//int button0Value = 0;
+uint8_t button1Value = 0;
 
 float pitch;
 float roll;
 float yaw;
 
-
-
 void setup() {
   Wire.begin(0, 2);
-  Serial.begin(115200);
+  //Serial.begin(115200);
   delay(10);
+
+  //pinMode(button0Pin, INPUT);
+  pinMode(button1Pin, INPUT);
 
   connectESP();
   connectMPU();
-  
 }
 
-void connectESP(){
+//Connects the ESP
+void connectESP() {
   //Connecting to the wifi network
-  Serial.println("\n\nConnecting to" + String(ssid));
+  //Serial.println("\n\nConnecting to" + String(ssid));
   //Sets ESP8266 to be a client explicitly
-  WiFi.mode(WIFI_STA); 
+  WiFi.mode(WIFI_STA);
   //Connects to the network
   WiFi.begin(ssid, password);
 
   //Tries to connect to a network as a client as many times
-  for(uint8_t attempt = 0; attempt < maxAttempsNetwork; ++attempt){
-    if(WiFi.status() == WL_CONNECTED) break;
+  for (uint8_t attempt = 0; attempt < maxAttempsNetwork; ++attempt) {
+    if (WiFi.status() == WL_CONNECTED) break;
     delay(500); //Try check 500ms later
-    Serial.printf("Attempt %d failed to connect to network\n", attempt);
+    //Serial.printf("Attempt %d failed to connect to network\n", attempt);
   }
 
   //Check for connection one more time and tries to connect to server
-  if(WiFi.status() == WL_CONNECTED){
-    Serial.println("WiFi connected to IP: " + client.localIP());
-    Serial.println("Trying to connect to host: " + String(host));
-    
+  if (WiFi.status() == WL_CONNECTED) {
+    //Serial.println("WiFi connected to IP: " + client.localIP());
+    //Serial.println("Trying to connect to host: " + String(host));
+
     //Attempts to connect to host
     uint8_t hostAttempt = 0;
-    for(; hostAttempt < maxAttempsHost; ++hostAttempt){
-      if(client.connect(host, port)){
+    for (; hostAttempt < maxAttempsHost; ++hostAttempt) {
+      if (client.connect(host, port)) {
         hostAttempt = 0; //Condition to check later
         break;
       }
-      Serial.printf("Attempt %d failed to connect to host\n", hostAttempt);
+      //Serial.printf("Attempt %d failed to connect to host\n", hostAttempt);
       delay(2000);
-    } 
-    
-    //It connected 
-    if(hostAttempt == 0) Serial.println("Fully connected");
-    else Serial.println("Failed to connect to host");
-  }
-  else Serial.println("WiFi could not connect");
-}
+    }
 
-void connectMPU(){
+    //It connected
+    if (hostAttempt == 0) hostAttempt = 1;//Serial.println("Fully connected");
+      else hostAttempt = 1;//Serial.println("Failed to connect to host");
+      }
+  //else hostAttempt = 1;//Serial.println("WiFi could not connect");
+  }
+//Connects the MPU
+void connectMPU() {
   byte c = myIMU.readByte(MPU9250_ADDRESS, WHO_AM_I_MPU9250);
   //Checks for connection of MPU
-  if(c == 0x71){
-    Serial.println("\nMPU9250 is online...");
+  if (c == 0x71) {
+    //Serial.println("\nMPU9250 is online...");
     myIMU.MPU9250SelfTest(myIMU.SelfTest);
     myIMU.calibrateMPU9250(myIMU.gyroBias, myIMU.accelBias);
     myIMU.initMPU9250();
@@ -100,29 +107,29 @@ void connectMPU(){
     myIMU.magscale[2] = 0.76;
   }
   else {
-    Serial.printf("Could not connect to MPU9250: 0x%x", c);
+    //Serial.printf("Could not connect to MPU9250: 0x%x", c);
   }
 }
-
-void readMPU(float& _pitch, float& _roll, float& _yaw){
-  if (myIMU.readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01){  
+// Reads value from the MPU
+void readMPU(float& _pitch, float& _roll, float& _yaw) {
+  if (myIMU.readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01) {
     myIMU.readAccelData(myIMU.accelCount);  // Read the x/y/z adc values
     myIMU.getAres();
 
     // Now we'll calculate the accleration value into actual g's
     // This depends on scale being set
-    myIMU.ax = (float)myIMU.accelCount[0]*myIMU.aRes; // - accelBias[0];
-    myIMU.ay = (float)myIMU.accelCount[1]*myIMU.aRes; // - accelBias[1];
-    myIMU.az = (float)myIMU.accelCount[2]*myIMU.aRes; // - accelBias[2];
+    myIMU.ax = (float)myIMU.accelCount[0] * myIMU.aRes; // - accelBias[0];
+    myIMU.ay = (float)myIMU.accelCount[1] * myIMU.aRes; // - accelBias[1];
+    myIMU.az = (float)myIMU.accelCount[2] * myIMU.aRes; // - accelBias[2];
 
     myIMU.readGyroData(myIMU.gyroCount);  // Read the x/y/z adc values
     myIMU.getGres();
 
     // Calculate the gyro value into actual degrees per second
     // This depends on scale being set
-    myIMU.gx = (float)myIMU.gyroCount[0]*myIMU.gRes;
-    myIMU.gy = (float)myIMU.gyroCount[1]*myIMU.gRes;
-    myIMU.gz = (float)myIMU.gyroCount[2]*myIMU.gRes;
+    myIMU.gx = (float)myIMU.gyroCount[0] * myIMU.gRes;
+    myIMU.gy = (float)myIMU.gyroCount[1] * myIMU.gRes;
+    myIMU.gz = (float)myIMU.gyroCount[2] * myIMU.gRes;
 
     myIMU.readMagData(myIMU.magCount);  // Read the x/y/z adc values
     myIMU.getMres();
@@ -131,9 +138,9 @@ void readMPU(float& _pitch, float& _roll, float& _yaw){
     // Include factory calibration per data sheet and user environmental
     // corrections
     // Get actual magnetometer value, this depends on scale being set
-    myIMU.mx = (float)myIMU.magCount[0]*myIMU.mRes*myIMU.magCalibration[0] - myIMU.magbias[0];
-    myIMU.my = (float)myIMU.magCount[1]*myIMU.mRes*myIMU.magCalibration[1] - myIMU.magbias[1];
-    myIMU.mz = (float)myIMU.magCount[2]*myIMU.mRes*myIMU.magCalibration[2] - myIMU.magbias[2];
+    myIMU.mx = (float)myIMU.magCount[0] * myIMU.mRes * myIMU.magCalibration[0] - myIMU.magbias[0];
+    myIMU.my = (float)myIMU.magCount[1] * myIMU.mRes * myIMU.magCalibration[1] - myIMU.magbias[1];
+    myIMU.mz = (float)myIMU.magCount[2] * myIMU.mRes * myIMU.magCalibration[2] - myIMU.magbias[2];
     myIMU.mx *= myIMU.magscale[0];
     myIMU.my *= myIMU.magscale[1];
     myIMU.mz *= myIMU.magscale[2];
@@ -151,18 +158,18 @@ void readMPU(float& _pitch, float& _roll, float& _yaw){
   // modified to allow any convenient orientation convention. This is ok by
   // aircraft orientation standards! Pass gyro rate as rad/s
   //MadgwickQuaternionUpdate(ax, ay, az, gx*PI/180.0f, gy*PI/180.0f, gz*PI/180.0f,  my,  mx, mz);
-    /*MahonyQuaternionUpdate(myIMU.ax, myIMU.ay, myIMU.az, myIMU.gx*DEG_TO_RAD,
-                         myIMU.gy*DEG_TO_RAD, myIMU.gz*DEG_TO_RAD, myIMU.my,
-                         myIMU.mx, myIMU.mz, myIMU.deltat);*/
-    MadgwickQuaternionUpdate(myIMU.ax, myIMU.ay, myIMU.az, myIMU.gx*DEG_TO_RAD,
-                         myIMU.gy*DEG_TO_RAD, myIMU.gz*DEG_TO_RAD, myIMU.my,
-                         myIMU.mx, myIMU.mz, myIMU.deltat);
+  /*MahonyQuaternionUpdate(myIMU.ax, myIMU.ay, myIMU.az, myIMU.gx*DEG_TO_RAD,
+                       myIMU.gy*DEG_TO_RAD, myIMU.gz*DEG_TO_RAD, myIMU.my,
+                       myIMU.mx, myIMU.mz, myIMU.deltat);*/
+  MadgwickQuaternionUpdate(myIMU.ax, myIMU.ay, myIMU.az, myIMU.gx * DEG_TO_RAD,
+                           myIMU.gy * DEG_TO_RAD, myIMU.gz * DEG_TO_RAD, myIMU.my,
+                           myIMU.mx, myIMU.mz, myIMU.deltat);
 
-    // Serial print and/or display at 0.5 s rate independent of data rates
-    myIMU.delt_t = millis() - myIMU.count;
+  // Serial print and/or display at 0.5 s rate independent of data rates
+  myIMU.delt_t = millis() - myIMU.count;
 
-    // Print delay in milliseconds
-    if (myIMU.delt_t > 50){
+  // Print delay in milliseconds
+  if (myIMU.delt_t > 50) {
     // Define output variables from updated quaternion---these are Tait-Bryan
     // angles, commonly used in aircraft orientation. In this coordinate system,
     // the positive z-axis is down toward Earth. Yaw is the angle between Sensor
@@ -179,65 +186,71 @@ void readMPU(float& _pitch, float& _roll, float& _yaw){
     // For more see
     // http://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
     // which has additional links.
-      myIMU.yaw   = atan2(2.0f * (*(getQ()+1) * *(getQ()+2) + *getQ() * *(getQ()+3)),
-                          *getQ() * *getQ() + *(getQ()+1) * *(getQ()+1) - *(getQ()+2) * *(getQ()+2) - *(getQ()+3) * *(getQ()+3));
-                    
-      myIMU.pitch = -asin(2.0f * (*(getQ()+1) * *(getQ()+3) - *getQ() *
-                    *(getQ()+2)));
-                    
-      myIMU.roll  = atan2(2.0f * (*getQ() * *(getQ()+1) + *(getQ()+2) * *(getQ()+3)),
-                          *getQ() * *getQ() - *(getQ()+1) * *(getQ()+1) - *(getQ()+2) * *(getQ()+2) + *(getQ()+3) * *(getQ()+3));
-                    
-      myIMU.pitch *= RAD_TO_DEG;
-      myIMU.yaw   *= RAD_TO_DEG;
-      // Declination of SparkFun Electronics (40°05'26.6"N 105°11'05.9"W) is
-      //   8° 30' E  ± 0° 21' (or 8.5°) on 2016-07-19
-      // - http://www.ngdc.noaa.gov/geomag-web/#declination
-      //myIMU.yaw   -= 0.331613;
-      myIMU.yaw   -= 1.9;
-      myIMU.roll  *= RAD_TO_DEG;
+    myIMU.yaw   = atan2(2.0f * (*(getQ() + 1) * *(getQ() + 2) + *getQ() * *(getQ() + 3)),
+                        *getQ() * *getQ() + * (getQ() + 1) * *(getQ() + 1) - * (getQ() + 2) * *(getQ() + 2) - * (getQ() + 3) * *(getQ() + 3));
 
-      //Saves values in the variables passed
-      _pitch = myIMU.pitch;
-      _roll  = myIMU.roll;
-      _yaw   = myIMU.yaw;
-      
-      myIMU.count = millis();
-      myIMU.sumCount = 0;
-      myIMU.sum = 0;
-    }
+    myIMU.pitch = -asin(2.0f * (*(getQ() + 1) * *(getQ() + 3) - *getQ() *
+                                *(getQ() + 2)));
+
+    myIMU.roll  = atan2(2.0f * (*getQ() * *(getQ() + 1) + * (getQ() + 2) * *(getQ() + 3)),
+                        *getQ() * *getQ() - * (getQ() + 1) * *(getQ() + 1) - * (getQ() + 2) * *(getQ() + 2) + * (getQ() + 3) * *(getQ() + 3));
+
+    myIMU.pitch *= RAD_TO_DEG;
+    myIMU.yaw   *= RAD_TO_DEG;
+    // Declination of SparkFun Electronics (40°05'26.6"N 105°11'05.9"W) is
+    //   8° 30' E  ± 0° 21' (or 8.5°) on 2016-07-19
+    // - http://www.ngdc.noaa.gov/geomag-web/#declination
+    //myIMU.yaw   -= 0.331613;
+    myIMU.yaw   -= 1.9;
+    myIMU.roll  *= RAD_TO_DEG;
+
+    //Saves values in the variables passed
+    _pitch = myIMU.pitch;
+    _roll  = myIMU.roll;
+    _yaw   = myIMU.yaw;
+
+    myIMU.count = millis();
+    myIMU.sumCount = 0;
+    myIMU.sum = 0;
+  }
 }
-
-//Sends message using ESP connection to host
-void sendMessage(String _message){
-  if(client.connected()) client.print(_message + "\r");
-  else Serial.println("Couldn't send message:\n " + _message + + "\nTo host\n");
+//Reads values from buttons
+void readButtons(int& _button1Value){//, int& _button1Value) {
+  //_button0Value = digitalRead(button0Pin);
+  _button1Value = digitalRead(button1Pin);
 }
-
 //Checks if ESP is still connected
-void checkESP(){
-  if(!client.connected()){
-    while (!client.connect(host, port)){
-      Serial.println("Knocked down");
+void checkESP() {
+  if (!client.connected()) {
+    while (!client.connect(host, port)) {
+      //Serial.println("Knocked down");
     }
   }
 }
 
+//Sends message using ESP connection to host
+void sendMessage(String _message) {
+  if (client.connected()) client.print(_message + "\r");
+  //else //Serial.println("Couldn't send message:\n " + _message + + "\nTo host\n");
+}
+
 void loop() {
-  Serial.println("Reading and sending once");
+  //Serial.println("Reading and sending once");
   readMPU(pitch, roll, yaw);
+  readButtons(button1Value);//, button1Value);
   String message = "";
   message += "ejeX:" + String(pitch) + ";";
   message += "ejeY:" + String(roll) + ";";
   message += "ejeZ:" + String(yaw) + ";";
+  message += "btn:" + String(button1Value) + ";";
   sendMessage(message);
-  Serial.println(message);
+  //Serial.println(message);
   checkESP();
   delay(interval);
 
   // Read all the lines of the reply from server and print them to Serial
   /*while (client.available()) {
     String line = client.readStringUntil('\r');
-    Serial.print(line);
+    //Serial.print(line);
   }*/
 }
--- NodeMCU/NodeMCU.ino
+++ NodeMCU/NodeMCU.ino
@@ -9,7 +9,7 @@ const char* ssid = "WiFiCar";
 const char* password = "pass1234";
 
 
-// servidor con el puerto y variable con la maxima cantidad de 
+// servidor con el puerto y variable con la maxima cantidad de
 
 WiFiServer server(PORT);
 WiFiClient serverClients[MAX_SRV_CLIENTS];
@@ -19,7 +19,7 @@ const long interval = 100;
 
 
 #define ldr D8
-#define ab  D7 
+#define ab  D7
 #define clk D6
 #define EnA D5 // 
 #define In1 D4 // D4 en HIGH : retroceder
@@ -36,7 +36,7 @@ const long interval = 100;
  * b: traseras
  * c: direccional izq
  * d: direccional der
- * e: 
+ * e:
  * f:
  * g:
  * h:
@@ -44,35 +44,35 @@ const long interval = 100;
 
 
 //
-bool Turn [2] = {LOW,HIGH};
-bool CarMove [2] = {HIGH,LOW};
+bool Turn [2] = {LOW, HIGH};
+bool CarMove [2] = {HIGH, LOW};
 int vel = 0;
 bool dir = 0;
 int battery = 0;
-             //abcd 
+//abcd
 byte luces = B00000000;
 
 void setup() {
   Serial.begin(115200);
-  pinMode(In1,OUTPUT);
-  pinMode(In2,OUTPUT);
-  pinMode(In3,OUTPUT);
-  pinMode(In4,OUTPUT);
-  pinMode(EnA,OUTPUT);
-  pinMode(EnB,OUTPUT);
-  pinMode(clk,OUTPUT);
-  pinMode(ab,OUTPUT);
-  
-  IPAddress ip(192,168,43,200);
-  IPAddress gateway(192,168,43,1);
-  IPAddress subnet(255,255,255,0);
+  pinMode(In1, OUTPUT);
+  pinMode(In2, OUTPUT);
+  pinMode(In3, OUTPUT);
+  pinMode(In4, OUTPUT);
+  pinMode(EnA, OUTPUT);
+  pinMode(EnB, OUTPUT);
+  pinMode(clk, OUTPUT);
+  pinMode(ab, OUTPUT);
+
+  IPAddress ip(192, 168, 43, 200);
+  IPAddress gateway(192, 168, 43, 1);
+  IPAddress subnet(255, 255, 255, 0);
 
   WiFi.config(ip, gateway, subnet);
 
   WiFi.mode(WIFI_STA);
 
   WiFi.begin(ssid, password);
-  
+
   uint8_t i = 0;
   while (WiFi.status() != WL_CONNECTED && i++ < 20) delay(500);
   if (i == 21) {
@@ -88,7 +88,7 @@ void setup() {
 }
 
 void loop() {
-  
+
   unsigned long currentMillis = millis();
   uint8_t i;
   //check if there are any new clients
@@ -110,164 +110,164 @@ void loop() {
     previousMillis = currentMillis;
     for (i = 0; i < MAX_SRV_CLIENTS; i++) {
       if (serverClients[i] && serverClients[i].connected()) {
-        if(serverClients[i].available()){
+        if (serverClients[i].available()) {
           String mensaje = serverClients[i].readStringUntil('\r');
           serverClients[i].flush();
-          String respuesta; 
+          String respuesta;
           process(mensaje, &respuesta);
           Serial.println(mensaje);
           serverClients[i].println(respuesta);
           serverClients[i].stop();
-        }  
+        }
       }
     }
   }
-  digitalWrite(In3,Turn[0]);
-  digitalWrite(In4,Turn[1]);  
-  digitalWrite(In1,CarMove[0]);
-  digitalWrite(In2,CarMove[1]);
+  digitalWrite(In3, Turn[0]);
+  digitalWrite(In4, Turn[1]);
+  digitalWrite(In1, CarMove[0]);
+  digitalWrite(In2, CarMove[1]);
   digitalWrite(EnB, dir);
   analogWrite(EnA, vel);
 }
 
-void process(String input, String * output){
+void process(String input, String * output) {
   Serial.println("Checking input....... ");
   int comienzo = 0, delComa, del2puntos;
   bool result = false;
-  delComa = input.indexOf(';',comienzo);
-  
-  while(delComa>0){
+  delComa = input.indexOf(';', comienzo);
+
+  while (delComa > 0) {
     String comando = input.substring(comienzo, delComa);
     Serial.print("Processing comando: ");
     Serial.println(comando);
     del2puntos = comando.indexOf(':');
-    
-    if(del2puntos>0){
-        String llave = comando.substring(0,del2puntos);
-        String valor = comando.substring(del2puntos+1);
-        Serial.print("(llave, valor) = ");
-        Serial.print(llave);
-        Serial.println(valor);
-        *output = definir(llave,valor); 
+
+    if (del2puntos > 0) {
+      String llave = comando.substring(0, del2puntos);
+      String valor = comando.substring(del2puntos + 1);
+      Serial.print("(llave, valor) = ");
+      Serial.print(llave);
+      Serial.println(valor);
+      *output = definir(llave, valor);
     }
-    else if(comando == "sense"){
-      *output = getSense();         
-    }     
-    comienzo = delComa+1;
-    delComa = input.indexOf(';',comienzo);
+    else if (comando == "sense") {
+      *output = getSense();
+    }
+    comienzo = delComa + 1;
+    delComa = input.indexOf(';', comienzo);
   }
 }
 
-String definir(String llave, String valor){
-  String result="ok";;
+String definir(String llave, String valor) {
+  String result = "ok";;
   Serial.print("Comparing llave: ");
   Serial.println(llave);
-  if(llave == "pwm"){
+  if (llave == "pwm") {
     Serial.println("Move....");
     vel = valor.toInt();
-    if(vel<0){
-      vel*=-1;
+    if (vel < 0) {
+      vel *= -1;
       reverse();
     }
-    else{
+    else {
       forward();
     }
   }
- 
-  else if(llave == "dir"){
-    switch (valor.toInt()){
-      case 1:
-        Serial.println("Girando derecha");
-        turnright();
-        break;
-      case -1:
-        Serial.println("Girando izquierda");
-        turnleft();
-        break;
-       default:
-        Serial.println("directo");
-        straight();
-        break;
+
+  else if (llave == "dir") {
+    switch (valor.toInt()) {
+    case 1:
+      Serial.println("Girando derecha");
+      turnright();
+      break;
+    case -1:
+      Serial.println("Girando izquierda");
+      turnleft();
+      break;
+    default:
+      Serial.println("directo");
+      straight();
+      break;
     }
   }
-  else if(llave[0] == 'l'){
+  else if (llave[0] == 'l') {
     Serial.println("Cambiando Luces");
     bool encender = true;
     byte myBit = B00000000;
-    if(valor == "0") encender = false;
-    switch (llave[1]){
-      case 'f':
-        Serial.println("Luces delanteras");
-        myBit = B01111111;      
-        break;
-      case 'b':
-        Serial.println("Luces traseras");
-        myBit = B10111111;
-        break;
-      case 'l':
-        Serial.println("Luces izquierda");
-        myBit = B11011111;
-        break;
-      case 'r':
-        Serial.println("Luces derechas");
-        myBit = B11101111;
-        break;
-      default:
-        Serial.println("Ninguna de las anteriores");
-        break;
+    if (valor == "0") encender = false;
+    switch (llave[1]) {
+    case 'f':
+      Serial.println("Luces delanteras");
+      myBit = B01111111;
+      break;
+    case 'b':
+      Serial.println("Luces traseras");
+      myBit = B10111111;
+      break;
+    case 'l':
+      Serial.println("Luces izquierda");
+      myBit = B11011111;
+      break;
+    case 'r':
+      Serial.println("Luces derechas");
+      myBit = B11101111;
+      break;
+    default:
+      Serial.println("Ninguna de las anteriores");
+      break;
     }
     Serial.print("Despues SwitchCase");
-    Serial.println(myBit,BIN);
-    if(encender) luces = luces & myBit;
-    else{
+    Serial.println(myBit, BIN);
+    if (encender) luces = luces & myBit;
+    else {
       myBit = ~myBit;
       Serial.print("~myBit: ");
-      Serial.println(myBit,BIN);
-      luces = luces|myBit;
+      Serial.println(myBit, BIN);
+      luces = luces | myBit;
     }
     Serial.print("luces: ");
     Serial.println(luces, BIN);
     shiftOut(ab, clk, LSBFIRST, luces);
-  } 
-  else{
+  }
+  else {
     result = "Recived undefined key value: " + llave;
     Serial.println(result);
   }
   return result;
 }
 
-void straight(){
+void straight() {
   dir = 0;
 }
 
-void turnright(){
+void turnright() {
   dir = HIGH;
   Turn[0] = HIGH;
   Turn[1] = LOW;
 }
 
-void turnleft(){
+void turnleft() {
   dir = HIGH;
   Turn[0] = LOW;
   Turn[1] = HIGH;
 }
 
-void forward(){
+void forward() {
   CarMove[0] = LOW;
   CarMove[1] = HIGH;
 }
 
-void reverse(){
+void reverse() {
   CarMove[0] = HIGH;
   CarMove[1] = LOW;
 }
 
-void stopCar(){
+void stopCar() {
   vel = 0;
   dir = 0;
 }
 
-String getSense(){
+String getSense() {
   int batteryLvl = analogRead(A0);
   int light = digitalRead(ldr);
   char sense [16];
